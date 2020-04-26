bf581f926bd6b5573eb3d688e4ab8bf3eb7a55d0
phyk <ppeter1@smail.uni-koeln.de>
2020-04-26 10:48:41 +0200

    Update Tests to match requirements

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -26,6 +26,7 @@ describe('JWT', () => {
 
       expect(decoded).toEqual(body);
     });
+    
     it(`can successfully decode a body without knowing the secret for algorithm ${algorithm}`, () => {
       const body = { foo: 'bar' };
       
