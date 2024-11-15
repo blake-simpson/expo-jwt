6063b05f0efffddf82f4178ce592a7b74028b9d1
guozichun <guotingchaopr@gmail.com>
2024-11-15 23:24:26 +0800

    :art: Improve code formatting and add a new helper function
    - Refactored import statements to use double quotes consistently
    - Added urlSafeBase64ToBase64 function in helpers.ts for URL-safe base64 conversion
    - Updated parse function in decoder.ts to handle URL-safe base64 encoding
    - Included isUrlSafeBase64 function in helpers.ts for checking URL-safe base64 content

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -1,7 +1,15 @@
 export const urlEncodeBase64 = (signature: string): string => {
-  signature = signature.replace(/(=+)$/g, '');
-  signature = signature.replace(/\//g, '_');
-  signature = signature.replace(/\+/g, '-');
+  signature = signature.replace(/(=+)$/g, "");
+  signature = signature.replace(/\//g, "_");
+  signature = signature.replace(/\+/g, "-");
 
   return signature;
 };
+
+export const urlSafeBase64ToBase64 = (signature) => {
+  if (isUrlSafeBase64(signature))
+    return signature.replace(/-/g, "+").replace(/_/g, "/");
+};
+
+export const isUrlSafeBase64 = (content: string) =>
+  /^[A-Za-z0-9_-]*[.=]{0,2}$/.test(content);
