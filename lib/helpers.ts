1502520994fdd56bfe448849554a790dc63ea14e
guozichun <guotingchaopr@gmail.com>
2024-11-26 15:18:28 +0800

    change code style

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -1,14 +1,14 @@
 export const urlEncodeBase64 = (signature: string): string => {
-  signature = signature.replace(/(=+)$/g, "");
-  signature = signature.replace(/\//g, "_");
-  signature = signature.replace(/\+/g, "-");
+  signature = signature.replace(/(=+)$/g, '');
+  signature = signature.replace(/\//g, '_');
+  signature = signature.replace(/\+/g, '-');
 
   return signature;
 };
 
 export const urlSafeBase64ToBase64 = (signature) => {
   if (isUrlSafeBase64(signature))
-    return signature.replace(/-/g, "+").replace(/_/g, "/");
+    return signature.replace(/-/g, '+').replace(/_/g, '/');
 };
 
 export const isUrlSafeBase64 = (content: string) =>
